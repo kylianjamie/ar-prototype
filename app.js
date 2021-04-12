2bf65a94e27129fd0c7dbfa533cdb562e8035084
kylianjamie <kjnijnuis@gmail.com>
2021-04-12 17:26:16 +0200

    Styling and pan test

--- app.js
+++ app.js
@@ -83,43 +83,44 @@ class App{
         this.scene.add( this.controller );
   
         //pan event:
-        // let touchDown, touchX, touchY, deltaX, deltaY;
+        let touchDown, touchX, touchY, deltaX, deltaY;
+        const hotspot = document.getElementById('pan-hotspot');
 
-        // this.renderer.domElement.addEventListener('touchstart', function(e){
-        //     console.log('touch start');
-        //     e.preventDefault();
-        //     touchDown = true;
-        //     touchX = e.touches[0].pageX;
-        //     touchY = e.touches[0].pageY;
-        // }, false);
+        hotspot.addEventListener('touchstart', function(e){
+            console.log('touch start');
+            e.preventDefault();
+            touchDown = true;
+            touchX = e.touches[0].pageX;
+            touchY = e.touches[0].pageY;
+        }, false);
 
-        // this.renderer.domElement.addEventListener('touchend', function(e){
-        //     console.log('touchend');
-        //     e.preventDefault();
-        //     touchDown = false;
-        // }, false);
+        hotspot.addEventListener('touchend', function(e){
+            console.log('touchend');
+            e.preventDefault();
+            touchDown = false;
+        }, false);
 
-        // this.renderer.domElement.addEventListener('touchmove', function(e){
-        //     e.preventDefault();
+        hotspot.addEventListener('touchmove', function(e){
+            e.preventDefault();
             
-        //     if(!touchDown){
-        //         return;
-        //     }
+            if(!touchDown){
+                return;
+            }
 
-        //     deltaX = e.touches[0].pageX - touchX;
-        //     deltaY = e.touches[0].pageY - touchY;
-        //     touchX = e.touches[0].pageX;
-        //     touchY = e.touches[0].pageY;
+            deltaX = e.touches[0].pageX - touchX;
+            deltaY = e.touches[0].pageY - touchY;
+            touchX = e.touches[0].pageX;
+            touchY = e.touches[0].pageY;
 
-        //     rotateObject();
+            rotateObject();
 
-        // }, false);
+        }, false);
 
-        // function rotateObject(){
-        //     if(self.chair){
-        //         self.chair.rotation.y += deltaX / 100;
-        //     }
-        // }
+        function rotateObject(){
+            if(self.chair){
+                self.chair.rotation.y += deltaX / 100;
+            }
+        }
     }
 	
     resize(){
