4279c0253156b6b5024ebed502e14b1b2277fbda
kylianjamie <kjnijnuis@gmail.com>
2021-04-12 16:13:42 +0200

    test

--- app.js
+++ app.js
@@ -82,43 +82,7 @@ class App{
         
         // this.scene.add( this.controller );
   
-        let touchDown, touchX, touchY, deltaX, deltaY;
-
-        this.uiElement.addEventListener('touchstart', function(e){
-            console.log('touch start');
-            e.preventDefault();
-            touchDown = true;
-            touchX = e.touches[0].pageX;
-            touchY = e.touches[0].pageY;
-        }, false);
-
-        this.uiElement.addEventListener('touchend', function(e){
-            console.log('touchend');
-            e.preventDefault();
-            touchDown = false;
-        }, false);
-
-        this.uiElement.addEventListener('touchmove', function(e){
-            e.preventDefault();
-            
-            if(!touchDown){
-                return;
-            }
-
-            deltaX = e.touches[0].pageX - touchX;
-            deltaY = e.touches[0].pageY - touchY;
-            touchX = e.touches[0].pageX;
-            touchY = e.touches[0].pageY;
-
-            rotateObject();
-
-        }, false);
-
-        function rotateObject(){
-            if(self.chair){
-                self.chair.rotation.y += deltaX / 100;
-            }
-        }
+        
     }
 	
     resize(){
@@ -195,6 +159,44 @@ class App{
             domOverlay: { root: uiElement }
         };
         
+        let touchDown, touchX, touchY, deltaX, deltaY;
+
+        uiElement.addEventListener('touchstart', function(e){
+            console.log('touch start');
+            e.preventDefault();
+            touchDown = true;
+            touchX = e.touches[0].pageX;
+            touchY = e.touches[0].pageY;
+        }, false);
+
+        uiElement.addEventListener('touchend', function(e){
+            console.log('touchend');
+            e.preventDefault();
+            touchDown = false;
+        }, false);
+
+        uiElement.addEventListener('touchmove', function(e){
+            e.preventDefault();
+            
+            if(!touchDown){
+                return;
+            }
+
+            deltaX = e.touches[0].pageX - touchX;
+            deltaY = e.touches[0].pageY - touchY;
+            touchX = e.touches[0].pageX;
+            touchY = e.touches[0].pageY;
+
+            rotateObject();
+
+        }, false);
+
+        function rotateObject(){
+            if(self.chair){
+                self.chair.rotation.y += deltaX / 100;
+            }
+        }
+
         function onSessionStarted( session ) {
 
             session.addEventListener( 'end', onSessionEnded );
