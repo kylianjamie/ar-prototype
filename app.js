deca304e6dac57572f13ae52142d2b5f1e08ef25
kylianjamie <kjnijnuis@gmail.com>
2021-04-12 11:28:14 +0200

    Target location

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ class App{
         
         const self = this;
         
-        loader.load( '../../assets/hdr/venice_sunset_1k.hdr', ( texture ) => {
+        loader.load( '/assets/hdr/venice_sunset_1k.hdr', ( texture ) => {
           const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
           pmremGenerator.dispose();
 
