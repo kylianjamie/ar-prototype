4f0ac1a5ead2edafab66625762ef9d1c0896d866
kylianjamie <kjnijnuis@gmail.com>
2021-04-13 12:23:31 +0200

    Test swipe down function

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,7 @@
 const infoOverlay = document.getElementById('info-overlay');
 const hammerInfo = new Hammer(infoOverlay);
 
-hammerInfo.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
-
-// hammerInfo.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+hammerInfo.get('swipe').set({ direction: Hammer.DIRECTION_DOWN });
 
 hammerInfo.on('swipedown', function() {
 	console.log('swipe down');
@@ -12,7 +10,3 @@ hammerInfo.on('swipedown', function() {
 hammerInfo.on('tap', function() {
 	console.log('tap');
 });
-
-// hammerInfo.on('swipe', function() {
-// 	console.log('swipe');
-// });
