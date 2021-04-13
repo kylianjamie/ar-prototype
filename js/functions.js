4a2d3021212a05d23e01eeea82ade968350d0afc
kylianjamie <kjnijnuis@gmail.com>
2021-04-13 13:39:10 +0200

    Slide in and out animations

--- js/functions.js
+++ js/functions.js
@@ -6,8 +6,16 @@ hammerInfo.get('swipe').set({ direction: Hammer.DIRECTION_DOWN });
 
 hammerInfo.on('swipedown', function() {
 	console.log('swipe down');
+    infoOverlay.classList.remove("animate__slideInUp");
+    infoOverlay.classList.add("animate__slideOutDown");
+
+    infoOverlay.addEventListener('animationend', () => {
+        infoOverlay.classList.add("hidden");
+        infoOverlay.classList.remove("animate__animated", "animate__slideOutDown");
+      });
 });
 
 document.getElementById('info-button').addEventListener("click", function() {
-    document.getElementById('info-overlay').classList.remove("hidden");
+    infoOverlay.classList.add("animate__animated", "animate__slideInUp");
+    infoOverlay.classList.remove("hidden");
 });
