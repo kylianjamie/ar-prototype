0416491bb3daddf9aba3cd4b4d06a0270c0b4d17
kylianjamie <kjnijnuis@gmail.com>
2021-04-13 13:46:24 +0200

    Animation debugging

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,25 @@
 // info overlay show and hide functions
 const infoOverlay = document.getElementById('info-overlay');
+const infoContainer = document.getElementById('info-container');
 const hammerInfo = new Hammer(infoOverlay);
 
 hammerInfo.get('swipe').set({ direction: Hammer.DIRECTION_DOWN });
 
 hammerInfo.on('swipedown', function() {
 	console.log('swipe down');
-    infoOverlay.classList.remove("animate__slideInUp");
-    infoOverlay.classList.add("animate__slideOutDown");
+    infoContainer.classList.add("animate__slideOutDown");
 
-    infoOverlay.addEventListener('animationend', () => {
+    infoContainer.addEventListener('animationend', () => {
         infoOverlay.classList.add("hidden");
-        infoOverlay.classList.remove("animate__animated", "animate__slideOutDown");
+        infoContainer.classList.remove("animate__slideOutDown");
       });
 });
 
 document.getElementById('info-button').addEventListener("click", function() {
-    infoOverlay.classList.add("animate__animated", "animate__slideInUp");
+    infoContainer.classList.add("animate__slideInUp");
     infoOverlay.classList.remove("hidden");
+
+    infoContainer.addEventListener('animationend', () => {
+        infoContainer.classList.remove("animate__slideInUp");
+      });
 });
