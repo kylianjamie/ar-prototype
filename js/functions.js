1412d44b25f755cf3c123f1b7862893788ab4860
kylianjamie <kjnijnuis@gmail.com>
2021-04-13 13:57:01 +0200

    Debugging animation

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,7 @@
 // info overlay show and hide functions
 const infoOverlay = document.getElementById('info-overlay');
 const infoContainer = document.getElementById('info-container');
+let infoOverlayStatus = 'hidden';
 const hammerInfo = new Hammer(infoOverlay);
 
 hammerInfo.get('swipe').set({ direction: Hammer.DIRECTION_DOWN });
@@ -8,18 +9,22 @@ hammerInfo.get('swipe').set({ direction: Hammer.DIRECTION_DOWN });
 hammerInfo.on('swipedown', function() {
 	console.log('swipe down');
     infoContainer.classList.add("animate__slideOutDown");
-
-    infoContainer.addEventListener('animationend', () => {
-        infoOverlay.classList.add("hidden");
-        infoContainer.classList.remove("animate__slideOutDown");
-      });
+    infoOverlayStatus = 'hidden';
 });
 
 document.getElementById('info-button').addEventListener("click", function() {
     infoContainer.classList.add("animate__slideInUp");
     infoOverlay.classList.remove("hidden");
-
-    infoContainer.addEventListener('animationend', () => {
-        infoContainer.classList.remove("animate__slideInUp");
-      });
+    infoOverlayStatus = 'visible';
 });
+
+infoContainer.addEventListener('animationend', () => {
+    if (infoOverlayStatus === 'visible'){
+        infoContainer.classList.remove("animate__slideInUp");
+    }
+
+    if (infoOverlayStatus === 'hidden'){
+        infoOverlay.classList.add("hidden");
+        infoContainer.classList.remove("animate__slideOutDown");  
+    }
+  });
