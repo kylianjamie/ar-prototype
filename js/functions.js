f38bd5b50248f0ef19f7201f0d822a7a01a5c89a
kylianjamie <kjnijnuis@gmail.com>
2021-04-13 13:30:47 +0200

    Info overlay open on click

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,4 @@
+// info overlay show and hide functions
 const infoOverlay = document.getElementById('info-overlay');
 const hammerInfo = new Hammer(infoOverlay);
 
@@ -5,4 +6,8 @@ hammerInfo.get('swipe').set({ direction: Hammer.DIRECTION_DOWN });
 
 hammerInfo.on('swipedown', function() {
 	console.log('swipe down');
+});
+
+document.getElementById('info-button').addEventListener("click", function() {
+    document.getElementById('info-overlay').classList.remove("hidden");
 });
