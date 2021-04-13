069372d3bdd81c2649c1814e58edb9586db26788
kylianjamie <kjnijnuis@gmail.com>
2021-04-13 12:21:28 +0200

    Swipe test again

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,18 @@
 const infoOverlay = document.getElementById('info-overlay');
 const hammerInfo = new Hammer(infoOverlay);
 
-hammerInfo.on('pandown', function() {
-	console.log('pan down');
+hammerInfo.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
+
+// hammerInfo.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+
+hammerInfo.on('swipedown', function() {
+	console.log('swipe down');
 });
 
 hammerInfo.on('tap', function() {
 	console.log('tap');
 });
 
-hammerInfo.on('swipe', function() {
-	console.log('swipe');
-});
+// hammerInfo.on('swipe', function() {
+// 	console.log('swipe');
+// });
