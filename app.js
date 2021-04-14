092334e4fd3f8a4c9fd22781d49ee23a7164d81d
kylianjamie <kjnijnuis@gmail.com>
2021-04-14 14:20:53 +0200

    Texture test

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ class App{
 
         this.reticle = new THREE.Mesh(
             new THREE.PlaneBufferGeometry( 0.5, 0.5, 32 ).rotateX( - Math.PI / 2 ),
-            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('/img/texture2.png') })
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('/img/texture2.png'), transparent: true })
         );
         
         this.reticle.matrixAutoUpdate = false;
