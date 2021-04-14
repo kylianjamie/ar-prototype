40a23d8697184b6805c1bb768cea07b37d9099d6
kylianjamie <kjnijnuis@gmail.com>
2021-04-14 10:24:03 +0200

    Changed swipedown into panstart down

--- js/functions.js
+++ js/functions.js
@@ -9,7 +9,7 @@ const hammerBlock = new Hammer(popupBlock);
 
 let overlayStatus = 'hidden';
 
-hammerOverlay.get('swipe').set({ direction: Hammer.DIRECTION_DOWN });
+hammerOverlay.get('pan').set({ direction: Hammer.DIRECTION_DOWN });
 hammerBlock.get('pan').set({ direction: Hammer.DIRECTION_HORIZONTAL });
 
 document.getElementById('info-button').addEventListener("click", function() {
@@ -28,9 +28,8 @@ function openOverlay() {
     overlayStatus = 'visible';
 }
 
-hammerOverlay.on('swipedown', closeOverlay);
-hammerBlock.on('panstart', closeOverlay);
-popupBlock.addEventListener("click", closeOverlay);
+hammerOverlay.on('panstart', closeOverlay);
+hammerBlock.on('panstart tap', closeOverlay);
 document.getElementById('container-bar').addEventListener("click", closeOverlay);
 
 function closeOverlay() {
