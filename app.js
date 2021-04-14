1c623bcc457042c386a84f905827734b1b72753e
kylianjamie <kjnijnuis@gmail.com>
2021-04-14 14:23:38 +0200

    Plane texture size

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ class App{
         // );
 
         this.reticle = new THREE.Mesh(
-            new THREE.PlaneBufferGeometry( 1, 1 ).rotateX( - Math.PI / 2 ),
-            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('/img/texture2.png'), transparent: true })
+            new THREE.PlaneBufferGeometry( 0.75, 0.75 ).rotateX( - Math.PI / 2 ),
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('/img/reticle-texture.png'), transparent: true })
         );
         
         this.reticle.matrixAutoUpdate = false;
