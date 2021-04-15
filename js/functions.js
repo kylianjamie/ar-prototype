bc3e9bf531ca076487ad6f4e4354565daa864e15
kylianjamie <kjnijnuis@gmail.com>
2021-04-15 11:27:07 +0200

    Cart button function

--- js/functions.js
+++ js/functions.js
@@ -69,6 +69,7 @@ popupContainer.addEventListener('animationend', () => {
 const addCartBtn = document.getElementById('add-card-btn') 
 let cartBtnClicked = false;
 const checkSVG = '<svg xmlns="http://www.w3.org/2000/svg" id="check-svg" class="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>'
+const goToCart = document.getElementById('go-to-cart');
 
 function addToCart() {
     if (!cartBtnClicked){
@@ -76,6 +77,18 @@ function addToCart() {
         addCartBtn.classList.remove('text-white', 'bg-gray-600');
         addCartBtn.classList.add('text-gray-700', 'border', 'border-gray-600');
         addCartBtn.innerHTML = 'Toegevoegd ' + checkSVG;
+
+        if (goToCart.classList.contains('hidden')){
+            goToCart.classList.remove('hidden');
+        }
+        
+        setTimeout(function(){
+            addCartBtn.innerHTML = 'In winkelwagen';
+            addCartBtn.classList.remove('text-gray-700', 'border', 'border-gray-600');
+            addCartBtn.classList.add('text-white', 'bg-gray-600');
+            cartBtnClicked = false;
+        }, 1000);
+
     }
 }
 
