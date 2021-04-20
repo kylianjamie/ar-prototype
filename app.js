dfc9319364a97e34a8b1490ef5b6052d6b94b2e7
kylianjamie <kjnijnuis@gmail.com>
2021-04-20 09:09:31 +0200

    domoverlay test

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ class App{
                         el.style.display = 'flex';
 
                         console.log('huts');
-                        console.log(XRSession);
+                        console.log(XRSession.domOverlay);
                     });
                 }
 			} );
